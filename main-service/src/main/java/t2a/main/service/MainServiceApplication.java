2e2e0c00ce0ea44bda710663a1d10198ccc7991f
anh0701 <quynhanhphamthi01@gmail.com>
2024-07-15 21:22:03 +0700

    init

---
+++ main-service/src/main/java/t2a/main/service/MainServiceApplication.java
@@ -0,0 +1,13 @@
+package t2a.main.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+
+}
